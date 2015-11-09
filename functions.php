859ed724dab91b321f6f2b5e3a22e235bfd40b79
Steven Park <info@stevenpark.co.uk>
2015-11-09 11:50:22 +0000

    Combine scripts to tools.js and minimise

--- functions.php
+++ functions.php
@@ -116,9 +116,7 @@ add_action( 'widgets_init', 'mezzanine_widgets_init' );
 function mezzanine_scripts() {
 	wp_enqueue_style( 'mezzanine-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'mezzanine-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
-	wp_enqueue_script( 'mezzanine-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( 'mezzanine-navigation', get_template_directory_uri() . '/js/min/tools-min.js', array(), '', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
