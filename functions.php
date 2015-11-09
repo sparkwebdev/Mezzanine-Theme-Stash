7a811874b1c8f5a4bb3a4ec54ae1d0988e82cbad
Steven Park <info@stevenpark.co.uk>
2015-11-09 11:40:10 +0000

    Remove customizer code (inc. custom header), inc/extras.php, inc/jetpack.php

--- functions.php
+++ functions.php
@@ -127,26 +127,6 @@ function mezzanine_scripts() {
 add_action( 'wp_enqueue_scripts', 'mezzanine_scripts' );
 
 /**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
-
-/**
- * Custom functions that act independently of the theme templates.
- */
-require get_template_directory() . '/inc/extras.php';
-
-/**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer.php';
-
-/**
- * Load Jetpack compatibility file.
- */
-require get_template_directory() . '/inc/jetpack.php';
