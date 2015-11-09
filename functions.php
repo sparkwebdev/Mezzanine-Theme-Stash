068313ca9d2e62ed372f7a4394b3b200b7d16e39
Steven Park <info@stevenpark.co.uk>
2015-11-09 11:54:26 +0000

    Rename tools script handle

--- functions.php
+++ functions.php
@@ -116,7 +116,7 @@ add_action( 'widgets_init', 'mezzanine_widgets_init' );
 function mezzanine_scripts() {
 	wp_enqueue_style( 'mezzanine-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'mezzanine-navigation', get_template_directory_uri() . '/js/min/tools-min.js', array(), '', true );
+	wp_enqueue_script( 'mezzanine-tools', get_template_directory_uri() . '/js/min/tools-min.js', array(), '', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
